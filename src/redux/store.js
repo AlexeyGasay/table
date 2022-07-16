02870a3af5499e8e8663759f9dbf0554b2bf8e5d
Alexey Indyuchenko <46484506+AlexeyGasay@users.noreply.github.com>
2022-07-16 12:51:19 +0300

    Delete src directory

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import tableDataSlice from "./tableDataSlice"
-
-export const store = configureStore({
-  reducer: {
-    table: tableDataSlice,
-  },
-});
